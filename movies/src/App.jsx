497e45d8f57ed82f5a6a57bda86705f6fdbd8e47
Cecilie <cjacob20@student.aau.dk>
2025-11-26 16:10:47 +0100

    Add profile and help to App.jsx

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Compare from "./pages/Compare";
+import Profile from "./pages/Profile";
+import Help from "./pages/Help";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -12,6 +14,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/compare/:userId" element={<Compare />} />
+        <Route path="/profile/:userId" element={<Profile />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </div>
   );
